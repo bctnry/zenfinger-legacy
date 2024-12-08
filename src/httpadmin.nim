93d2a0b7bf3b117600f32ac2c9b3ee00cf3a63fd
Sebastian Zack Tin Lahm <bctnry@sdf.org>
2024-12-08 20:02:02 +0000

    update

--- src/httpadmin.nim
+++ src/httpadmin.nim
@@ -5,6 +5,21 @@ import contentresolve
 from std/strutils import parseInt, startsWith
 from htmlgen as html import nil
 
+proc renderAdminPage(config: ZConfig): string =
+  let siteName = config.getConfig(CONFIG_GROUP_HTTP, CONFIG_KEY_HTTP_SITE_NAME)
+  return (
+    html.html(
+      html.head(
+        html.meta(charset="utf-8"),
+        html.title("admin panel :: " & siteName),
+      ),
+      html.body(
+        
+      )
+    )
+  )
+      
+
 
 proc handleAdmin*(req: Request, config: ZConfig) {.async.} =
-  await req.respond(Http200, "Shh... admin page not done yet!", {"Content-Type": "text/html; charset=utf-8"})
+  await req.respond(Http200, "Shh... admin page not done yet!", {"Content-Type": "text/html; charset=utf-8"}.newHttpHeaders())
--- src/zenfinger.nim
+++ src/zenfinger.nim
@@ -1,33 +1,52 @@
 import std/[asyncnet, asyncdispatch, asynchttpserver]
 import std/random
+import std/net
+import std/cmdline
 import fingerserver
 import httpserver
 import config
 import log
 import ask
 import install
+from std/strutils import rsplit, strip, join
 
 randomize()
 
-if not initConfig():
-  asyncCheck log("Asking for initialization...")
-  let c = askWithChoice("It seems like you don't have a config file. Create one?", @["y","n"])
-  if c == "n":
-    asyncCheck log("Initialization denied. Exiting.")
-    echo "Initialization denied. Exiting."
-    quit(0)
+when isMainModule:
+  if paramCount() <= 0:
+    if not initConfig():
+      asyncCheck log("Asking for initialization...")
+      let c = askWithChoice("It seems like you don't have a config file. Create one?", @["y","n"])
+      if c == "n":
+        asyncCheck log("Initialization denied. Exiting.")
+        echo "Initialization denied. Exiting."
+        quit(0)
+      else:
+        install()
+        let r = initConfig()
+        if not r:
+          asyncCheck log("Failed to load config. Exiting.")
+          echo "Failed to load config. Exiting."
+          quit(0)
+
+    let c = getCurrentConfig()
+    asyncCheck serveFinger(c)
+    asyncCheck serveHTTP(c)
+    runForever()
   else:
-    install()
-    let r = initConfig()
-    if not r:
-      asyncCheck log("Failed to load config. Exiting.")
-      echo "Failed to load config. Exiting."
-      quit(0)
-
-let c = getCurrentConfig()
-
-asyncCheck serveFinger(c)
-asyncCheck serveHTTP(c)
-runForever()
-
+    # this is the client part. we'll do it with blocking sockets.
+    let q = paramStr(1).strip().rsplit('@', maxsplit=1)
+    let query = q[0]
+    let server = if q.len <= 1: "127.0.0.1" else: q[1]
+    let socket = newSocket()
+    socket.connect(server, 79.Port)
+    socket.send(query & "\r\n")
+    var res: seq[string] = @[]
+    var s: string
+    while true:
+      socket.readLine(s)
+      if s == "": break
+      elif s == "\r\n": res.add("")
+      else: res.add(s)
+    echo res.join("\n")
 
